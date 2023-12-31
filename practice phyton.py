b0095967d5b8ec6f00d6ec7500c06f65a6de6a44
anee127 <anni76371@gmail.com>
2023-12-31 18:23:24 +0500

    neutral

---
+++ practice phyton.py
@@ -0,0 +1,10 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hellow,phyton")
+hellow,phyton
+>>> age=23
+>>> print("age")
+age
+>>> age="56"
+>>> print(age)
+56
